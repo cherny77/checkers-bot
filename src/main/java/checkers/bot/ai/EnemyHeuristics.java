0f5b1a7256680dc78ea4d2139d18e47360398301
GgrlHtOU <i11am22from2000ukraine@gmail.com>
2020-12-12 03:33:53 +0200

    feat: add enemy heuristics

--- src/main/java/checkers/bot/ai/EnemyHeuristics.java
+++ src/main/java/checkers/bot/ai/EnemyHeuristics.java
@@ -1,21 +1,32 @@
 package checkers.bot.ai;
 
+import checkers.bot.api.IHeuristic;
+
 import static checkers.bot.util.Constants.*;
 import static checkers.bot.util.Constants.ENEMY_QUEEN;
 
-public class EnemyHeuristics {
+public class EnemyHeuristics implements IHeuristic {
 
+    private int[][] board;
     private int[][] originBoard;
-    private int[] boardCheckers = new int[] {0,0,0,0};
-    /* enemySimpleChecker, enemySimpleQueen, goodSimpleChecker, goodSimpleQueen*/
+    private int[] boardCheckers = new int[]{0, 0, 0, 0};
+    private int[] originBoardCheckers = new int[]{0, 0, 0, 0};
 
     public EnemyHeuristics(int[][] originBoard) {
         this.originBoard = originBoard;
         countCheckers(this.originBoard, boardCheckers);
     }
 
-    public int getEnemyHeuristic(){
-        return boardCheckers[0] + boardCheckers[1] * QUEEN_KOEFF - boardCheckers[2] - boardCheckers[3] * QUEEN_KOEFF;
+    @Override
+    public int estimate(int[][] original, int[][] state) {
+        this.originBoard = original;
+        this.board = state;
+        countCheckers(this.board, this.boardCheckers);
+        countCheckers(this.originBoard, this.originBoardCheckers);
+
+        return boardCheckers[2] + boardCheckers[3] * QUEEN_KOEFF - boardCheckers[0] - boardCheckers[1] * QUEEN_KOEFF
+                + isBecomeQueen(originBoardCheckers, boardCheckers, 3)
+                + countHowManyWeHaveKilledEnemy(originBoardCheckers, boardCheckers, 0);
     }
 
     private void countCheckers(int[][] board, int[] boardCheckers) {
@@ -34,14 +45,11 @@ public class EnemyHeuristics {
         }
     }
 
+    private int isBecomeQueen(int[] originBoardCheckers, int[] boardCheckers, int queenPos) {
+        return originBoardCheckers[queenPos] < boardCheckers[queenPos] ? QUEEN_KOEFF : 0;
+    }
 
-
-
-
-
-
-
-
-
-
+    private int countHowManyWeHaveKilledEnemy(int[] originBoardCheckers, int[] boardCheckers, int figurePosition) {
+        return originBoardCheckers[figurePosition] - boardCheckers[figurePosition] + (originBoardCheckers[figurePosition + 1] - boardCheckers[figurePosition + 1]) * QUEEN_KOEFF;
+    }
 }
