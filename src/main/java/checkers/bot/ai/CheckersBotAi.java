1ae93607b568bd352dde3984f2acecac52d5c15a
cherny77 <kyrylo.chornokozynskyi@ukma.edu.ua>
2020-12-12 03:48:47 +0200

    added heuristics

--- src/main/java/checkers/bot/ai/CheckersBotAi.java
+++ src/main/java/checkers/bot/ai/CheckersBotAi.java
@@ -9,13 +9,15 @@ import checkers.bot.util.Move;
 import java.util.*;
 
 public class CheckersBotAi implements ICheckersBotAi {
-    public static int DEPTH = 3;
+    public static int DEPTH = 6;
     private IHeuristic heuristic;
+    private IHeuristic helpingHeuristic;
     private String color;
 
 
-    public CheckersBotAi(IHeuristic heuristic) {
+    public CheckersBotAi(IHeuristic heuristic, IHeuristic helpingHeuristic) {
         this.heuristic = heuristic;
+        this.helpingHeuristic = helpingHeuristic;
     }
 
     @Override
@@ -60,8 +62,39 @@ public class CheckersBotAi implements ICheckersBotAi {
 
         }
         if (possVals.size() == 1)
-        return possVals.get(0);
-        return possVals.get(0);
-//        if (isMax)
+            return possVals.get(0);
+        Map<int[][], Integer> finVals = new HashMap<>();
+        List<int[][]> finPossVals = new ArrayList<>();
+        if (isMax) {
+            for (int[][] board : possVals) {
+                finVals.put(board, helpingHeuristic.estimate(curState, board));
+            }
+
+            int maxVal = Collections.max(finVals.values());
+            for (int[][] n : possVals) {
+                if (finVals.get(n) == maxVal) finPossVals.add(n);
+            }
+        } else {
+            for (int[][] board : possVals) {
+                finVals.put(board, helpingHeuristic.estimate(curState, board));
+            }
+
+            int minVal = Collections.min(finVals.values());
+            for (int[][] n : possVals) {
+                if (finVals.get(n) == minVal) finPossVals.add(n);
+            }
+        }
+
+        if (finPossVals.size() == 1) finPossVals.get(0);
+        return getRandom(possVals);
+    }
+
+
+    private int[][] getRandom(List<int[][]> possVal) {
+        Random random = new Random();
+        int ind = random.nextInt(possVal.size());
+        return possVal.get(ind);
     }
 }
+
+
